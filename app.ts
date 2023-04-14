a5e1a5e5294aa8804b4bedf274f7529ff4f9d073
Julia Rudenko <yrudenko@i.ua>
2023-04-14 12:37:49 +0300

    Task 3

--- app.ts
+++ app.ts
@@ -1,108 +1,108 @@
-// const button = document.querySelector("button")! as HTMLButtonElement;
-// const input1 = document.getElementById("num1")! as HTMLInputElement;
-// const input2 = document.getElementById("num2")! as HTMLInputElement;
+const button = document.querySelector("button")! as HTMLButtonElement;
+const input1 = document.getElementById("num1")! as HTMLInputElement;
+const input2 = document.getElementById("num2")! as HTMLInputElement;
 
-// function add(num1: number, num2: number) {
-//   return num1 + num2;
-// }
+function add(num1: number, num2: number) {
+  return num1 + num2;
+}
 
 // button.addEventListener("click", function () {
 //   console.log(add(+input1.value, +input2.value));
 // });
 
-// let age:number;
-// age = 50;
-// let name: string;
-// name = 'Max';
-// let toggle: boolean;
-// toggle = true;
-// let empty:null;
-// empty = null;
-// let notInitialize:undefined;
-// notInitialize = undefined;
-// let callback = (a: number) => number;
-// callback = (a) => { return 100 + a };
+// // let age:number;
+// // age = 50;
+// // let name: string;
+// // name = 'Max';
+// // let toggle: boolean;
+// // toggle = true;
+// // let empty:null;
+// // empty = null;
+// // let notInitialize:undefined;
+// // notInitialize = undefined;
+// // let callback = (a: number) => number;
+// // callback = (a) => { return 100 + a };
 
 
-// --any--
-let anything: any;
-anything = -20;
-anything = 'Text';
-anything = {};
+// // --any--
+// let anything:any;
+// anything = -20;
+// anything = 'Text';
+// anything = {};
 
-// --unknown--
-let some: unknown;
-some = 'Text';
-let str: string;
-if (typeof some === "string") {
-  str = some;
-}
+// // --unknown--
+// let some:unknown;
+// some = 'Text';
+// let str: string;
+// if (typeof some === "string") {
+//   str = some;
+// }
   
-// --Tuple--
-let person: [string, number]
-person = ['Max', 21];
+// // --Tuple--
+// let person: [string, number]
+// person = ['Max', 21];
 
-// --Enum--
-enum Load {LOADING, READY};
-const page = {
-  load: Load.READY
-}
-if (page.load === Load.LOADING) {
-  console.log('Page is loading');
-}
-if (page.load === Load.READY) {
-  console.log('Page loaded');
-}
+// // --Enum--
+// enum Load {LOADING, READY};
+// const page = {
+//   load: Load.READY
+// }
+// if (page.load === Load.LOADING) {
+//   console.log('Page is loading');
+// }
+// if (page.load === Load.READY) {
+//   console.log('Page loaded');
+// }
 
-// --Union Type--
+// // --Union Type--
 
-let union: string | number;
-union = 2;
-union = "Julia";
+// let union: string | number;
+// union = 2;
+// union = "Julia";
 
-// --Literal Type--
+// // --Literal Type--
 
-let literal: 'enable' | 'disable';
-literal = 'enable';
-literal = 'disable';
+// let literal: 'enable' | 'disable';
+// literal = 'enable';
+// literal = 'disable';
 
-// Types for a function 
-function showMessage(message:string) {
-  console.log(message);
-}
-function calc(num1:number, num2:number) {
-  return num1 + num2;
-}
-function customError():never {
-  throw new Error('Error');
-}
+// // Types for a function
+// function showMessage(message:string) {
+//   console.log(message);
+// }
+// function calc(num1:number, num2:number) {
+//   return num1 + num2;
+// }
+// function customError():never {
+//   throw new Error('Error');
+// }
 
-// Создайте свой тип данных на основе имеющихся данных.
-const page1 = {
-  title: 'The awesome page',
-  likes: 100,
-  accounts: ['Max', 'Anton', 'Nikita'],
-  status: 'open',
-  details: {
-    createAt: '2021-01-01',
-    updateAt: '2021-05-01',
-  }
-}
+// // Создайте свой тип данных на основе имеющихся данных.
+// const page1 = {
+//   title: 'The awesome page',
+//   likes: 100,
+//   accounts: ['Max', 'Anton', 'Nikita'],
+//   status: 'open',
+//   details: {
+//     createAt: '2021-01-01',
+//     updateAt: '2021-05-01',
+//   }
+// }
 
-const page2 = {
-  title: 'Python or Js',
-  likes: 5,
-  accounts: ['Alex'],
-  status: 'close',
-}
+// const page2 = {
+//   title: 'Python or Js',
+//   likes: 5,
+//   accounts: ['Alex'],
+//   status: 'close',
+// }
 
-type Page = {
-  title: string;
-  likes: number;
-  accounts: string[];
-  status: 'open' | 'close';
-  details?: {
-    createAt: Date,
-    updateAt: Date,
-  }
-}
+// type Page = {
+//   title: string;
+//   likes: number;
+//   accounts: string[];
+//   status: 'open' | 'close';
+//   details?: {
+//     createAt: Date,
+//     updateAt: Date,
+//   }
+// }
